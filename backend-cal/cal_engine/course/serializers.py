cdba08e8b96f1e3a8a1d17e591929da45e8c035e
Aditya <adityaburgula123@gmail.com>
2024-12-04 21:10:47 +0000

    Begin API implementation

--- backend-cal/cal_engine/course/serializers.py
+++ backend-cal/cal_engine/course/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Course, Module, Section, SectionItem
 from cal_engine.institution.models import Institution
 
-class ModuleSerializer(serializers.ModelSerializer):
+
+class SectionItemSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Module model.
+    Serializer for the SectionItem model.
     """
     class Meta:
-        model = Module
+        model = SectionItem
         exclude = ('created_at', 'updated_at')
+
 
 class SectionSerializer(serializers.ModelSerializer):
     """
@@ -18,6 +20,26 @@ class SectionSerializer(serializers.ModelSerializer):
         model = Section
         exclude = ('created_at', 'updated_at')
 
+
+class ModuleSectionSummarySerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Section model to be used in ModuleSerializer.
+    """
+    class Meta:
+        model = Section
+        fields = ('id', 'title', 'description', 'sequence')
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    sections = ModuleSectionSummarySerializer(many=True, read_only=True)
+    """
+    Serializer for the Module model.
+    """
+    class Meta:
+        model = Module
+        fields = ('id', 'title', 'description', 'sequence', 'sections')
+
+
 class CourseModuleSummarySerializer(serializers.ModelSerializer):
     """
     Serializer for the Module model to be used in CourseSerializer.
@@ -26,6 +48,7 @@ class CourseModuleSummarySerializer(serializers.ModelSerializer):
         model = Module
         fields = ('id', 'title','description', 'sequence')
 
+
 class CourseInstitutionSummarySerializer(serializers.ModelSerializer):
     """
     Serializer for the Institution model to be used in CourseSerializer.
@@ -33,6 +56,7 @@ class CourseInstitutionSummarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Institution
         fields = ('id', 'name', 'description')
+
 
 class CourseSerializer(serializers.ModelSerializer):
     """
@@ -44,11 +68,3 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ('id', 'name', 'visibility', 'institution_details', 'description', 'modules')
 
-
-class SectionItemSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the SectionItem model.
-    """
-    class Meta:
-        model = SectionItem
-        exclude = ('created_at', 'updated_at')
