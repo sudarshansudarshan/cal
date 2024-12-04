c495dff583267c1b1cb4129d0450b6fb4213ebaf
Pranay Deep <pranaydeep139@gmail.com>
2024-12-04 13:01:50 +0000

    videos update (wip)

--- backend-cal/cal_engine/study_content/admin.py
+++ backend-cal/cal_engine/study_content/admin.py
@@ -5,12 +5,12 @@ from .models import Video, VideoSegment, Article
 
 class VideoSegmentInline(admin.TabularInline):
     model = VideoSegment
-    fields = ('title', 'start_time', 'assessment', 'transcript', "youtube_id")
+    fields = ('title', 'start_time', 'assessment', 'transcript', "video")
     extra = 0
 
 
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'section', 'created_at', 'updated_at')
+    list_display = ('title', 'section', 'description', 'created_at', 'updated_at', 'link', 'youtube_id')
     inlines = [VideoSegmentInline]
 
 class ArticleAdmin(admin.ModelAdmin):
