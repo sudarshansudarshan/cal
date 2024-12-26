f2ed371edf21f13650e276eb47a6ea1e49e0de3b
Aneesh Rao <sidrao2006@gmail.com>
2024-12-26 19:54:39 +0000

    Refactor Video model to use ItemTypeChoices for item_type assignment; update imports for consistency and clarity.

--- backend/lms_engine/core/course/models/__init__.py
+++ backend/lms_engine/core/course/models/__init__.py
@@ -1,7 +1,7 @@
 from .course import Course, CourseInstructor, VisibilityChoices
 from .module import Module
 from .section import Section
-from .section_item import SectionItem
+from .section_item import SectionItem, ItemTypeChoices
 from .video import Video
 from .article import Article
 from .source import Source
