62f465490ff5b6754058859ef806d0c4fa67a734
Pranay Deep <pranaydeep139@gmail.com>
2025-02-08 11:07:57 +0530

    Refactored AI Engine - With Local Ollama Service Alongside Gemini
    Co-authored-by: farhat-qadri <22f2000927@ds.study.iitm.ac.in>

--- backend/ai_engine/app/services.py
+++ backend/ai_engine/app/services.py
@@ -1,6 +1,9 @@
-from app.question_gen.main import process_video
-from app.question_gen.services.url_service import get_urls_from_playlist
+from app.question_generation.services import VideoProcessor, PlaylistProcessor, GeminiService, OllamaService
 from typing import List
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # def process_question_generation(transcript: str):
 #     return generate_question(transcript)
@@ -8,15 +11,26 @@ from typing import List
 # def process_answer_question(question: str):
 #     return answer_question(question)
 
+# ai_service = GeminiService()
+playlist_processor = PlaylistProcessor()
+# video_processor = VideoProcessor(ai_service)
+
 async def process_process_video(url: str, user_api_key: str, timestamps: List[int],
                           segment_wise_q_no: List[int],
                           segment_wise_q_model: List[str]):
-    result = await process_video(url, user_api_key, timestamps, segment_wise_q_no,
+    if user_api_key == "ollama1064":
+        ai_service = OllamaService()
+        print("Using OLLAAAAMMMAAAAAAAAAAA AI")
+    else:
+        ai_service = GeminiService()
+        ai_service.set_api_key(os.getenv("API_KEY"))
+    video_processor = VideoProcessor(ai_service)
+    result = await video_processor.process_video(url, user_api_key, timestamps, segment_wise_q_no,
                          segment_wise_q_model)
     return result
 
 async def get_urls(url: str):
-    urls = await get_urls_from_playlist(url)
+    urls = await playlist_processor.get_urls_from_playlist(url)
     return urls
 # Compare this snippet from app/routers/video.py:
 # from fastapi import APIRouter
